2d5a03687f35b55e73fcdb92cce3fea9921895fd
chebishev <atanas.chebishev@gmail.com>
2024-01-08 16:42:27 +0200

    edit README.md add output photo refactor and comments

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ReadTextFromImage
 OCR Program that reads text from image
 Used libraries:
-PIL and pytesseract
+PIL and pytesseract, easyocr
 
 You need to add the path to the image first and to run the file. 
 Windows users must install Tesseract from https://github.com/UB-Mannheim/tesseract/wiki
@@ -9,4 +9,10 @@ Windows users must install Tesseract from https://github.com/UB-Mannheim/tessera
 ![Image](devices_weekly_23051301.png)
 
 
-![Image](console-output.png)
+![Image](tesseract_output.png)
+
+--------------------------------------
+![Image](devices_weekly_23122407.png)
+
+
+![Image](easyocr_output.png)
--- easyocr_engine.py
+++ easyocr_engine.py
@@ -1,21 +1,11 @@
-# import easyocr
-from ocr_result import devices_list
+import easyocr
 
-# reader = easyocr.Reader(['en'])
-# result_weekly = reader.readtext('devices_weekly_23122407.png')
-# result_stable = reader.readtext('devices_stable_v14_23111222.png')
+reader = easyocr.Reader(['en'])
 
-devices = devices_list[6:]
-weekly_devices = {
-    "code_name": "",
-    "market_name": "",
-    "rom_name": "",
-    "rom_options": "",
-    "slug": ""
-}
-# for index in range(0, len(devices), 5):
-#     data_index = 1
-#     print(devices[index][data_index], devices[index+1][data_index], devices[index+2][data_index])
-last_codename = ""
-last_rom_name = ""
-stop_words = ("Global", "China", "Hybrid", "Delayed")
+# The output is a list in format [([[176, 4], [348, 4], [348, 28], [176, 28]], 'Supported devices', 0.9999310169899559)]
+# which means [[x1,y1],[x2,y2],[x3,y3],[x4,y4]], text, confidence
+result = reader.readtext('devices_weekly_23122407.png')
+
+# for better readability:
+for row in result:
+    print(row)
